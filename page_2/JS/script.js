9024cd35dfa7a4eaa25cd8029a64c5f922fa4092
heorhii_nosal <nocalko@gmail.com>
2025-03-15 18:09:55 +0200

    last-commit

--- page_2/JS/script.js
+++ page_2/JS/script.js
@@ -2,8 +2,6 @@ const resize =function()
 {
 const width =window.innerWidth;
 const height = window.innerHeight;
-console.log(width);
-console.log(height);
 gorizontal_size=(width*0.85)/120;
 vertical_size=(height*0.85)/80;
 document.documentElement.style.setProperty('--size',`${Math.min(gorizontal_size,vertical_size)}px`);
