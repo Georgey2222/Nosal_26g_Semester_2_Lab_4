d7cb7c613c91f4b0e77811262a9e2c8ef2638efc
heorhii_nosal <nocalko@gmail.com>
2025-03-15 21:21:24 +0200

    commit-10

--- JS/script_0.js
+++ JS/script_0.js
@@ -1,6 +1,5 @@
 const width =document.documentElement.clientWidth;
 const height = document.documentElement.clientHeight;
-console.log(width);
 gorizontal_size=(width*0.85)/240;
 vertical_size=(height*0.85)/80;
 document.documentElement.style.setProperty('--size',`${Math.min(gorizontal_size,vertical_size,10)}px`);
